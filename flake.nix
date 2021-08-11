c7ba0b791909d9a76f6ecdab08a0d8f20615f183
Joseph Lucas <lucas.jojo@gmail.com>
2021-08-11 17:46:29 +0200

    add buggy check

--- flake.nix
+++ flake.nix
@@ -11,11 +11,11 @@
         propagatedBuildInputs = [ pkgs.libmilter ];
         doCheck = false; # to avoid the missing `makemap` executable of Sendmail
         pythonImportsCheck = [ "Milter" ];
+        meta.description = ''A python extension module to enable python scripts to attach to Sendmail's libmilter API, enabling filtering of messages as they arrive. Since it's a script, you can do anything you want to the message - screen out viruses, collect statistics, add or modify headers, etc. You can, at any point, tell Sendmail to reject, discard, or accept the message.
+          Additional python modules provide for navigating and modifying MIME parts, and sending DSNs or doing CBVs.'';
       };
       defaultPackage.x86_64-linux = self.packages.x86_64-linux.pymilter;
-      devShell = pkgs.mkShell { inputsFrom = builtins.attrValues self.system; packages = [ pkgs ]; };
-      meta.longDescription = ''
-        A python extension module to enable python scripts to attach to Sendmail's libmilter API, enabling filtering of messages as they arrive. Since it's a script, you can do anything you want to the message - screen out viruses, collect statistics, add or modify headers, etc. You can, at any point, tell Sendmail to reject, discard, or accept the message.
-        Additional python modules provide for navigating and modifying MIME parts, and sending DSNs or doing CBVs.'';
-    };
+      checks.x86_64-linux.pymilter = self.packages.x86_64-linux.pymilter;
+      devShell = pkgs.mkShell { inputsFrom = builtins.attrValues self.system; packages = [ pkgs.pymilter ]; };
+  };
 }
